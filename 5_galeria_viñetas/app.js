a3b6955d0a48824c5ef5ffe2d327929de795da44
Oquendo07 <alejooquendo468@gmail.com>
2025-09-01 16:14:11 -0500

    5_galeria_viñetas

--- 5_galeria_viñetas/app.js
+++ 5_galeria_viñetas/app.js
@@ -67,3 +67,104 @@ btnAnterior.addEventListener("click", function () {
 
    mostrarEscena(indice)
 })
+
+miniaturas.forEach(function (miniatura, i) {
+    miniatura.addEventListener("click", function () {
+        mostrarEscena(i)
+    })
+})
+
+const playStopImg1 = document.querySelector('#play-stop-img1');
+const playStopImg2 = document.querySelector('#play-stop-img2');
+const audio1 = document.querySelector('#audio-1');
+const audio2 = document.querySelector('#audio-2');
+
+
+console.log(playStopImg1);
+console.log(playStopImg2);
+
+//Funcion para reproducir video
+function reproducirVideo() {
+    video.play();
+}
+
+
+//Funcion para detener video
+function pausarVideo  (){
+    video.pause();
+    
+}
+
+
+function toggleAudioUno() {
+    if (audio1.paused) {
+        audio1.play();
+        playStopImg1.textContent = '⏸️ Stop';
+    } else {
+        audio1.pause();
+        playStopImg1.textContent = '▶️ Play';
+    }
+    
+}
+playStopImg1.addEventListener('click', toggleAudioUno);
+
+
+
+function toggleAudioDos() {
+    if (audio2.paused) {
+        audio2.play();
+        playStopImg2.textContent = '⏸️ Stop';
+    } else {
+        audio2.pause();
+        playStopImg2.textContent = '▶️ Play';
+    }
+    
+}
+playStopImg2.addEventListener('click', toggleAudioDos);
+
+miniaturas.forEach( (miniatura, i)=> {
+    miniatura.addEventListener("click", function () {
+        mostrarEscena(i);
+    })
+})
+
+const audios = [
+document.getElementById("audio-escena1"), 
+document.getElementById("audio-escena2"),
+document.getElementById("audio-escena3"),
+];
+
+let audioActivo = null;
+let sonidoReproduciendo = false;
+
+function reproducirSonidoEscena (i) {
+// Detener audio anterior si existe
+if (audioActivo) {
+audioActivo.pause();
+audioActivo.currentTime  = 0;
+}
+if (sonidoReproduciendo) {
+audioActivo = audios [i];
+audioActivo.play();
+}
+}
+
+// Modificar función mostrarEscena para activar sonido 
+ const funcionoriginal = mostrarEscena;
+mostrarEscena = function(i) {
+funcionoriginal(i);
+reproducirSonidoEscena (i);
+};
+// Botón Play/Stop
+const btnPlayStop = document.getElementById("play-stop-img3");
+btnPlayStop.addEventListener ("click", function () { 
+    sonidoReproduciendo = !sonidoReproduciendo;
+btnPlayStop.textContent = sonidoReproduciendo ? "Stop" : "play";
+
+if (sonidoReproduciendo) {
+reproducirSonidoEscena(indice);
+}else if (audioActivo) {
+audioActivo.pause();
+}
+});
+
